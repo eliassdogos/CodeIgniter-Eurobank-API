0c870ac8625d56b3f9aebb0ab707e042998e8885
elias <elsdogos@gmail.com>
2018-03-29 18:40:26 +0300

    config

--- my_eurobank_config.php
+++ my_eurobank_config.php
@@ -6,8 +6,10 @@
  * @link
  * @filesource
  */
-$config['eurobank']['mid']     = "Your Merchant ID";
-$config['eurobank']['shared_secret']     = "Your Key";
-$config['eurobank']['form_action_post_url']     = "your URL"; // Productive or Test Env
-$config['eurobank']['confirmation_url']     = "your URL"; // url for live or production enviroment to Connect
-$config['eurobank']['cancel_url']     = "your URL"; //
+$config['eurobank']['mid']     = "Your Merchant ID"; // Required
+$config['eurobank']['shared_secret']     = "Your Key"; // Required
+$config['eurobank']['form_action_post_url']     = "your URL"; // // Required Productive or Test Env
+$config['eurobank']['confirmation_url']     = "your URL"; // // Required url for live or production enviroment to Connect
+$config['eurobank']['cancel_url']     = "your URL"; // // Required
+$config['eurobank']['lang']     = NULL; // Optional OR $config['eurobank']['lang']     = "Your Option";
+
