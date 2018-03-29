633143adb87c5f40f572dfe895464e83308e0b55
elias <elsdogos@gmail.com>
2018-03-29 14:05:19 +0300

    Create my_eurobank_config.php

---
+++ my_eurobank_config.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Name:    Eurobak API for Codeigniter
+ * Author:  Elias elsdogos
+ * Email: elsdogos@gmail.com
+ * @link
+ * @filesource
+ */
+$config['eurobank']['mid']     = "Your Merchant ID";
+$config['eurobank']['shared_secret']     = "Your Key";
+$config['eurobank']['form_action_post_url']     = "your URL"; // Productive or Test Env
+$config['eurobank']['confirmation_url']     = "your URL"; // url for live or production enviroment to Connect
+$config['eurobank']['cancel_url']     = "your URL"; //
